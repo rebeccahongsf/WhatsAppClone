fad165af89a96d89f7d1036aa3e693f85d62ebb4
Rebecca Hong <rebeccahongsf@gmail.com>
2021-02-17 21:45:40 -0800

    feat(chatsscreen): add ChatListItem component and modified ChatsScreen

---
+++ components/ChatListItem/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+import { ChatRoom } from '../../types';
+
+export type ChatListItemProps = {
+  chatRoom: ChatRoom;
+}
+
+const ChatListItem = (props: ChatListItemProps) => {
+  const { chatRoom } = props;
+  return (
+    <View style={ }>
+      <Avatar source={{ uri: chatRoom. }}
+      <Text>
+        {chatRoom.lastMessage.content}
+      </Text>
+    </View>
+  )
+};
+
+export default ChatListItem;
