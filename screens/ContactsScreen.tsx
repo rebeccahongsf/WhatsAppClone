5508bb8794166c489a9ab9bb3d1545b771e97815
Rebecca Hong <rebeccahongsf@gmail.com>
2021-04-09 16:33:05 -0700

    feat(contacts): display users within contacts

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,14 +1,33 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import { useState, useEffect } from 'react';
+import { FlatList } from 'react-native-gesture-handler';
+import API, { graphqlOperation } from '@aws-amplify/api';
+import { listUsers } from '../src/graphql/queries';
+
 import { View } from '../components/Themed';
 import ChatListItem from '../components/ChatListItem';
-
-import users from '../data/Users';
-import { FlatList } from 'react-native-gesture-handler';
 import NewMessageButton from '../components/NewMessageButton';
 import ContactListItem from '../components/ContactListItem';
 
 export default function Contacts() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersData = await API.graphql(graphqlOperation(
+          listUsers
+        ));
+        setUsers(usersData.data.listUsers.items);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchUsers();
+  }, [])
+
   return (
     <View style={{width: '100%', height: '100%'}}>
       <FlatList
